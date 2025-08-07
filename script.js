aaa33fda6f0e3d92a1a64212e0ea46a87ecb7613
Soraito <great.sky.110@gmail.com>
2025-08-07 22:52:11 +0900

    change: すべてHello Worldに変更

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ window.addEventListener("load", function() { // 修正: `addEvenListener` → `a
   const btn = document.getElementById("myButton"); // 修正: `getElementByID` → `getElementById`
 
   btn.addEventListener("click", function() { // 修正: `adEventListener` → `addEventListener`
-    alert("You clicked the button!"); // 修正: セミコロン漏れ
+    alert("Hello World!!"); // 修正: セミコロン漏れ
   });
 });
