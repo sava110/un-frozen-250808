c01842a719a202d7efb31399fbc0c74d6bca9366
naruse-4869 <9684itmadedoiruprivate.eye@gmail.com>
2025-08-08 15:38:30 +0900

    git add .

---
+++ naruse/script.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function() { 
+  const btn = document.getElementById("myButton"); 
+
+  btn.addEventListener("click", function() { 
+    alert("You clicked the button!");
+  })
+})
--- script.js
+++
@@ -1,7 +0,0 @@
-window.addEvenListener("load", function() { 
-  const btn = document.getElementByID("myButton"); 
-
-  btn.adEventListener("click", function() { 
-    alert("You clicked the button!")
-  })
-})
