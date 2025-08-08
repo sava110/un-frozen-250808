785d085f73570496975c615ad64f1a69cfecfb78
Hayat <hayatooooo868686@gmail.com>
2025-08-08 13:50:07 +0900

    Fix HTML/CSS/JS errors

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ window.addEvenListener("load", function() {
   const btn = document.getElementByID("myButton"); 
 
   btn.adEventListener("click", function() { 
-    alert("You clicked the button!")
+    alert("You clicked the button!");
   })
 })
